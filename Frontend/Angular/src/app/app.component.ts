a6dc00d93ce42d1f10c82afc633d7b4a9e49f8a4
Aman Mishra <aman.mishra@taazaa.com>
2025-08-16 18:57:25 -0400

    Added frontend code

---
+++ Frontend/Angular/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  todoList = []; 
+
+    ngOnInit(): void {
+      this.getTodoList();
+    }
+
+   async getTodoList(){
+    const url = `/api/v1/todo/todoList`
+     const res = await fetch(url);
+     const result = await res.json();
+     this.todoList = result;
+     console.log({toodolist:this.todoList});
+    }
+}

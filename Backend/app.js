3a00c08ecee350315dd3b5f55b997c3f35a8f72a
Aman Mishra <aman.mishra@taazaa.com>
2025-08-17 07:02:56 -0400

    Added new route '/' to send welcome message

--- Backend/app.js
+++ Backend/app.js
@@ -20,17 +20,18 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(cookieParser())
 
 
-app.use('/',(req,res)=>{
-    console.log({req:req})
-    res.send(`<h1>Welcome to the Backend API! Please refer to the documentation for usage instructions.</h1>`
-    )
-});
-
 //Routes import
 import {userRouter} from './src/routes/user.routes.js';
 import { TodoRouter } from './src/routes/todo.routes.js';
 
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/todo',TodoRouter);
+
+app.get('/',(req,res)=>{
+    console.log({req:req})
+    res.send(`<h1>Welcome to the Backend API! Please refer to the documentation for usage instructions.</h1>`
+    )
+});
+
 
 export {app};
